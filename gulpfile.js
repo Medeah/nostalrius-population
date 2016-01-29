a98ad13de560408e339de025c9cf71ddc88f3ed4
Frederik Andersen <fhan13@student.aau.dk>
2016-01-29 12:11:00 +0100

    documentation, npm not working

--- gulpfile.js
+++ gulpfile.js
@@ -5,8 +5,6 @@ var concat = require('gulp-concat');
 var uglify = require('gulp-uglify');
 
 gulp.task('javascript', function() {
-  // Minify and copy all JavaScript (except vendor scripts)
-  // with sourcemaps all the way down
   return gulp.src(['node_modules/jquery-flot/jquery.flot.js',
                     'node_modules/jquery-flot/jquery.flot.time.js',
                     'node_modules/jquery-flot/jquery.flot.selection.js',
--- readme.md
+++ readme.md
@@ -1,1 +1,10 @@
-you will need node, python3 and sqlite3
+Check Dependencies:
+node --version
+python3 --version
+sqlite3 --version
+
+npm install
+npm initdb #initilise sqlite db
+npm js #build javascrip
+npm getdata
+npm serve #start webserver
