ce981233bb793f1a6dd5c54fd6c6fabff78999b8
Frederik Andersen <fhan13@student.aau.dk>
2016-02-07 22:18:46 +0100

    Offline

--- getpop.py
+++ getpop.py
@@ -7,9 +7,20 @@ scraper = create_scraper() # returns a requests.Session object
 r = scraper.get("https://en.nostalrius.org/")
 
 soup = BeautifulSoup(r.content, 'html.parser')
-orange = soup.find_all("span", class_="orange")
-pvp = int(orange[4].text)
-pve = int(orange[8].text)
+
+blocks = soup.find_all("div", class_="block")
+pvpblock = blocks[3].find_all("span")
+pveblock = blocks[4].find_all("span")
+
+if pvpblock[0].text == "Online":
+    pvp = int(pvpblock[4].text)
+else:
+    pvp = 0
+
+if pveblock[0].text == "Online":
+    pve = int(pveblock[4].text)
+else:
+    pve = 0
 
 with app.app_context():
     c = get_db()
